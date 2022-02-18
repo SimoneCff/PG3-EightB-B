2be43cb32b9c8fc0d99ef30f444ceabdd7712b60
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-02-18 14:10:12 +0100

    -Added Button

---
+++ Cient/src/it/EightBB/Client/SwingBB/Button.java
@@ -0,0 +1,8 @@
+package it.EightBB.Client.SwingBB;
+
+import javax.swing.*;
+
+public abstract interface Button {
+    public void setButton();
+    public void InitialiteButtonIntoFrame(JFrame F);
+}
