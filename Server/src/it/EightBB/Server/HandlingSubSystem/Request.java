db8714de457e32245023881c5b1851533e3d7bd4
simonecioffi <simone.cioffi001@studenti.uniparthenope.it>
2022-02-20 13:06:22 +0100

    -added COR from server side -implememnted Auth checking

--- Server/src/it/EightBB/Server/HandlingSubSystem/Request.java
+++ Server/src/it/EightBB/Server/HandlingSubSystem/Request.java
@@ -3,20 +3,22 @@ package it.EightBB.Server.HandlingSubSystem;
 import java.util.ArrayList;
 
 public class Request {
-    private String R_SubSyst;
-    private String R_Request;
+    private String SubSyst;
+    private String Request;
     private String data;
 
-    public Request(String subsystem, String request){
-        this.R_SubSyst = subsystem;
+    public Request(String subsystem, String request, String data){
+        this.SubSyst = subsystem;
+        this.Request = request;
+        this.data = data;
     }
 
     public String getSubSys() {
-        return this.R_SubSyst;
+        return this.SubSyst;
     }
 
     public String getRequest(){
-        return this.R_Request;
+        return this.Request;
     }
 
     public String getData() {
--- Server/src/it/EightBB/Server/Server.java
+++ Server/src/it/EightBB/Server/Server.java
@@ -1,5 +1,13 @@
 package it.EightBB.Server;
 
+import it.EightBB.Server.Authservice.AuthenticationChandler;
+import it.EightBB.Server.HandlingSubSystem.Request;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Server  {
     public static void main(String[] args){
         System.out.println("Initialized Server");
@@ -7,14 +15,15 @@ public class Server  {
         System.out.println("Socket Creation: Success");
         DatabaseProxy database = new DatabaseProxy();
         database.connect();
+        boolean x = true;
         System.out.println("Connection to Database: Success");
-
+        //Set Hanlder
+        AuthenticationChandler Auth = new AuthenticationChandler();
         //
-        System.out.println("Waiting connections...");
-
-
-
-        ProxyServer.end();
-        System.out.println("Server Shutdown");
+            System.out.println("Waiting connections...");
+            ArrayList<String> req = new ArrayList<String>(List.of(ProxyServer.read()));
+            Auth.handlerRequest(new Request(req.get(1),req.get(2),req.get(3)),ProxyServer);
+            ProxyServer.end();
+            System.out.println("Server Shutdown");
     }
 }
