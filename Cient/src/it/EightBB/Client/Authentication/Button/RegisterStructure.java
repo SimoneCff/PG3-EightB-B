1f90e0d00058238432297a4e5333031ce843d349
nicola.improta <nicola.improta002@studenti.uniparthenope.it>
2022-02-22 13:04:16 +0100

    - added RegisterStructure button

---
+++ Cient/src/it/EightBB/Client/Authentication/Button/RegisterStructure.java
@@ -0,0 +1,16 @@
+/** La classe RegisterStructure serve a specificare il bottone che riguarda la sezione della registrazione della
+ * struttura */
+
+package it.EightBB.Client.Authentication.Button;
+import it.EightBB.Client.SwingBB.Button;
+import javax.swing.*;
+public class RegisterStructure implements Button{
+    private JButton RegisterStruct=null;
+    public void setButton(){
+        RegisterStruct=new JButton("Registra struttura");
+        RegisterStruct.setBounds(50,100,70,20);
+    }
+    public void InitialiteButtonIntoFrame(JFrame F){
+        F.add(RegisterStruct);
+    }
+}
