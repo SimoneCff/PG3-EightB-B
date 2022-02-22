195dd02aae4257a3d53acba8010767c4e4db4995
nicola.improta <nicola.improta002@studenti.uniparthenope.it>
2022-02-22 12:48:43 +0100

    - added AllOwnerStructures button

---
+++ Cient/src/it/EightBB/Client/Authentication/Button/AllOwnerStructures.java
@@ -0,0 +1,16 @@
+/** La classe OwnerAllStructure serve a specificare il bottone che riguarda tutte le strutture registrate dal
+ * proprietario */
+
+package it.EightBB.Client.Authentication.Button;
+import it.EightBB.Client.SwingBB.Button;
+import javax.swing.*;
+public class AllOwnerStructures implements Button{
+    private JButton AllStructures=null;
+    public void setButton(){
+        AllStructures=new JButton("Strutture Registrate");
+        AllStructures.setBounds(50,100,70,20);
+    }
+    public void InitialiteButtonIntoFrame(JFrame F){
+        F.add(AllStructures);
+    }
+}
