ec8f7abf6214486e9595e76b6bd829fe145e9594
MarioGiordyx <mario.giordano002@studenti.uniparthenope.it>
2022-02-22 12:08:47 +0100

    - Added registration confirm button (RegConfirm) - Added JCalendar Library

---
+++ Cient/src/it/EightBB/Client/Authentication/Form/PrenotationOne.java
@@ -0,0 +1,12 @@
+package it.EightBB.Client.Authentication.Form;
+
+import com.toedter.calendar.JCalendar;
+import it.EightBB.Client.SwingBB.Form;
+import javax.swing.*;
+import java.awt.*;
+import java.lang.annotation.Inherited;
+
+public class PrenotationOne {
+    private JTextField research = null;
+
+}
