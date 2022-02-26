ede1ce5643785ef9f7d0a8d455bf092f26821340
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-02-26 19:49:43 +0100

    Added Memento

--- Cient/src/it/EightBB/Client/Powner/Button/RegisterStructure.java
+++ Cient/src/it/EightBB/Client/Powner/Button/RegisterStructure.java
@@ -3,12 +3,16 @@
 
 package it.EightBB.Client.Powner.Button;
 import it.EightBB.Client.Interface.Template.Button;
+import it.EightBB.Client.Powner.ConcreteHandlerThree;
+
 import javax.swing.*;
 public class RegisterStructure implements Button{
     private JButton RegisterStruct=null;
     public void setButton(){
         RegisterStruct=new JButton("Registra struttura");
         RegisterStruct.setBounds(20,340,150,60);
+        RegisterStruct.setActionCommand("Owner-B-Rowner");
+        RegisterStruct.addActionListener(ConcreteHandlerThree.getInstance());
     }
     public void InitialiteButtonIntoFrame(JFrame F){
         F.add(RegisterStruct);
