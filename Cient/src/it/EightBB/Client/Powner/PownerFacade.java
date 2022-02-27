7edcd705118d9b596250104f19ebb46ba27019fb
MarioGiordyx <mario.giordano002@studenti.uniparthenope.it>
2022-02-27 02:28:15 +0100

    - Added

--- Cient/src/it/EightBB/Client/Powner/PownerFacade.java
+++ Cient/src/it/EightBB/Client/Powner/PownerFacade.java
@@ -199,8 +199,8 @@ public class PownerFacade{
                 String[] label = L.get(i).split(",");
                 JLabel type = new JLabel(label[1]);
                 JLabel value = new JLabel(label[2]);
-                type.setBounds();
-                value.setBounds();
+                type.setBounds(50, 160, 100, 30);
+                value.setBounds(50, 220, 100, 30);
 
                 F.add(type);
                 F.add(value);
