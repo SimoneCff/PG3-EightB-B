03210fe9dd9b7b32eca9cbe05d0442cdaa9c84f2
MarioGiordyx <mario.giordano002@studenti.uniparthenope.it>
2022-02-27 03:55:45 +0100

    - Added

--- Cient/src/it/EightBB/Client/Powner/Form/DocumentTourism.java
+++ Cient/src/it/EightBB/Client/Powner/Form/DocumentTourism.java
@@ -8,20 +8,23 @@ import java.awt.*;
 import java.io.*;
 
 public class DocumentTourism extends Component implements Form {
-
+    private JLabel seleziona = null;
     private JButton selezionaFile = null;
 
     @Override
     public void setForm() {
         selezionaFile = new JButton("SELEZIONA");
+        seleziona = new JLabel("Selezionare il file da inviare alla questura/ente turismo");
 
         selezionaFile.setBounds(100, 100, 400, 400);
+       seleziona.setBounds(100, 160, 400, 400);
         selezionaFile.addActionListener(e -> {selezionaFile();});
     }
 
     @Override
     public void InitialiateFormIntoFrame(JFrame F) {
         F.add(selezionaFile);
+        F.add(seleziona);
 
         F.setSize(1366, 768);
         F.getContentPane().setBackground(new Color(225,204,204));
