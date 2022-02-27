23473c560d28e9a6718abe3f27495e21ce043f9e
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-02-27 04:46:11 +0100

    Added Memento

--- Server/src/it/EightBB/Server/pclient/pclientCHandler.java
+++ Server/src/it/EightBB/Server/pclient/pclientCHandler.java
@@ -1,8 +1,10 @@
 package it.EightBB.Server.pclient;
 
+import it.EightBB.Server.Database.DatabaseOperations;
 import it.EightBB.Server.HandlingSubSystem.Handler;
 import it.EightBB.Server.HandlingSubSystem.Request;
 import it.EightBB.Server.Powenr.PownerChandler;
+import it.EightBB.Server.Powenr.PownerDBOperations;
 import it.EightBB.Server.Proxy.SocketInitialService;
 
 import java.sql.SQLException;
@@ -12,12 +14,19 @@ public class pclientCHandler extends Handler {
 
     public static Handler getInstance(){
         if(instance == null){
-            instance = new PclientCHandler();
+            instance = new pclientCHandler();
         } return instance;
     }
 
     @Override
     public String handlerRequest(Request request, SocketInitialService socket) throws SQLException {
-        return null;
+        String Req = null;
+        if(request.getSubSys().equals("Client")){
+            DatabaseOperations PC = new PclientDBOperations();
+            switch (request.getRequest()){
+                case "Modify" : Req= PC.modifyQueryFromTable(request.getData().getTable(), request.getData());
+            }
+        }else Req= "False";
+        return  Req;
     }
 }
